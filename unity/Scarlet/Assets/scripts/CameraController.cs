63f5eee3c056fe7482a283f7685e3fd46eacafb4
Jonathan Brem <jon.brem@t-online.de>
2016-11-02 12:50:20 +0100

    Added a dummy arena with a dummy player, controls and camera.
    Just testing; will implement other camera modes (and controls) to see what feels natural.

---
+++ unity/Scarlet/Assets/scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float m_DampTime = 0.2f;
+    public float m_ScreenEdgeBuffer = 4f;
+    public float m_MinSize = 6.5f;
+
+    public GameObject m_Focus;
+    public GameObject m_Scarlet;
+
+    private Camera m_Camera;
+    private float m_ZoomSpeed;
+
+    private float m_ZoomLevel;
+    private float m_Angle;
+
+	// Use this for initialization
+	void Start () {
+        m_Camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // calculate camera position based on player X pos
+
+        float xPos = m_Scarlet.transform.position.x;
+        float yPos = m_Camera.transform.position.y;
+        float zPos = m_Scarlet.transform.position.z - m_Focus.transform.position.z - 5;
+
+        m_Camera.transform.position = new Vector3(xPos, yPos, zPos);
+        m_Camera.transform.LookAt(m_Focus.transform);
+	}
+}
---
+++ unity/Scarlet/Assets/scripts/PlayerControls.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float m_HorizontalInput;
+    public float m_VerticalInput;
+
+    public float m_Speed = 0.3f;
+
+    public Rigidbody m_RigidBody;
+
+    private void Awake ()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_HorizontalInput = Input.GetAxis("Horizontal");
+        m_VerticalInput = Input.GetAxis("Vertical");
+
+        Move();
+        Rotate();
+	}
+
+    // move scarlet in the right direction
+    void Move()
+    {
+        float normalizedSpeed = (Mathf.Abs(m_HorizontalInput) + Mathf.Abs(m_VerticalInput)) * m_Speed;
+        if (normalizedSpeed >= m_Speed)
+        {
+            normalizedSpeed = m_Speed;
+        }
+
+        Vector3 movement = new Vector3(m_HorizontalInput * normalizedSpeed, 0, m_VerticalInput * normalizedSpeed);
+        m_RigidBody.MovePosition(m_RigidBody.position + movement * Time.deltaTime);
+    }
+
+    // make sure scarlet is looking in the right direction
+    void Rotate()
+    {
+        // don't want to change where Scarlet is looking when she is not moving
+        if (Mathf.Abs(m_HorizontalInput) <= 0.1f && Mathf.Abs(m_VerticalInput) <= 0.1f) return;
+        
+
+        float angle = Mathf.Atan2(-m_VerticalInput, m_HorizontalInput);
+
+        Quaternion rotation = Quaternion.Euler(0f, Mathf.Rad2Deg * angle, 0f);
+        m_RigidBody.MoveRotation(rotation);
+    }
+
+}
