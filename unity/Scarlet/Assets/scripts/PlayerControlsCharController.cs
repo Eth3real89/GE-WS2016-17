e2e2547da8c8d31784e92afa97138ffd6d20c62b
Andreas Pritschet <andreas@pritschet.eu>
2016-11-06 11:15:29 +0100

    added dash to other scene

--- unity/Scarlet/Assets/scripts/PlayerControlsCharController.cs
+++ unity/Scarlet/Assets/scripts/PlayerControlsCharController.cs
@@ -8,9 +8,15 @@ public class PlayerControlsCharController : MonoBehaviour
     public float m_VerticalInput;
 
     public float m_Speed = 1.3f;
+    public float m_DashDistance;
+    public float m_DashSpeed;
+    public float m_DashCooldown;
 
-    public Rigidbody m_RigidBody;
+    private float m_LastDash;
 
+    public bool m_ControlsEnabled = true;
+
+    private Rigidbody m_RigidBody;
     private Animator animator;
 
     private void Awake()
@@ -28,11 +34,15 @@ public class PlayerControlsCharController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!m_ControlsEnabled)
+            return;
+
         m_HorizontalInput = Input.GetAxis("Horizontal");
         m_VerticalInput = Input.GetAxis("Vertical");
 
         Move();
         Rotate();
+        CheckDash();
     }
 
     // move scarlet in the right direction
@@ -62,4 +72,43 @@ public class PlayerControlsCharController : MonoBehaviour
         m_RigidBody.MoveRotation(rotation);
     }
 
+    private void CheckDash()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Time.time >= m_LastDash + m_DashCooldown)
+            {
+                Dash();
+                m_LastDash = Time.time;
+            }
+        }
+    }
+
+    private void Dash()
+    {
+        m_ControlsEnabled = false;
+        SetVisibility(false);
+
+        StartCoroutine(Blink());
+    }
+
+    private void SetVisibility(bool visible)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Renderer r = transform.GetChild(i).GetComponent<Renderer>();
+            if (r != null)
+                r.enabled = visible;
+        }
+    }
+
+    private IEnumerator Blink()
+    {
+        yield return new WaitForSeconds(m_DashSpeed);
+
+        m_RigidBody.MovePosition(m_RigidBody.transform.position + m_RigidBody.transform.forward * m_DashDistance);
+
+        m_ControlsEnabled = true;
+        SetVisibility(true);
+    }
 }
