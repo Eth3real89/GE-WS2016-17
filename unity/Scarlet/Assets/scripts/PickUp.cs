f3c63409423e6d366e0c8439365f0714fc552d02
Andreas Pritschet <andreas@pritschet.eu>
2016-11-27 21:29:09 +0100

    added sound to collectible

--- unity/Scarlet/Assets/scripts/PickUp.cs
+++ unity/Scarlet/Assets/scripts/PickUp.cs
@@ -2,11 +2,13 @@
 
 public class PickUp : MonoBehaviour {
     public PlaceCollectible pcScript;
+    public AudioClip pickUpSound;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(pickUpSound, Camera.main.transform.position);
             pcScript.hasCollectible = true;
             gameObject.SetActive(false);
             Destroy(this);
