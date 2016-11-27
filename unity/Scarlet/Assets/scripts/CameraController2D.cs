82160f78b1270da463108d13506603216c539aa1
Andreas Pritschet <andreas@pritschet.eu>
2016-11-27 14:49:20 +0100

    changed camera tracking behaviour so climbing animation can be seen

--- unity/Scarlet/Assets/scripts/CameraController2D.cs
+++ unity/Scarlet/Assets/scripts/CameraController2D.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraController2D : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     public float yOffset;
     public float zOffset;
     public float trackAhead;
@@ -12,11 +12,17 @@ public class CameraController2D : MonoBehaviour
 
     private float zeroY;
     private float trackAheadCurrent;
+    private float playerRunSpeed;
+    private Rigidbody playerRb;
+    private Transform playerT;
 
     void Start()
     {
-        zeroY = player.position.y;
-        transform.position = new Vector3(player.position.x, yOffset, zOffset);
+        playerRunSpeed = player.GetComponent<PlayerControlsCharController>().m_SpeedRun;
+        playerRb = player.GetComponent<Rigidbody>();
+        playerT = player.transform;
+        zeroY = playerT.position.y;
+        transform.position = new Vector3(playerT.position.x, yOffset, zOffset);
         transform.rotation = Quaternion.Euler(viewAngle, 0, 0);
     }
 
@@ -27,11 +33,10 @@ public class CameraController2D : MonoBehaviour
 
     private void Track()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        trackAheadCurrent = trackAhead * hInput;
+        trackAheadCurrent = trackAhead * (playerRb.velocity.x / playerRunSpeed);
         Vector3 targetPosition = transform.position;
-        targetPosition.x = player.position.x + trackAheadCurrent;
-        targetPosition.y = player.position.y - zeroY + yOffset;
+        targetPosition.x = playerT.position.x + trackAheadCurrent;
+        targetPosition.y = playerT.position.y - zeroY + yOffset;
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothingFactor * Time.deltaTime);
     }
 }
