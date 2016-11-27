1b750b61e142f0e128b5cd8ee2a3d9cf9433a1ed
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2016-11-27 20:59:40 +0100

    added main menu with transition to 25D scene

---
+++ unity/Scarlet/Assets/scripts/GameStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour {
+
+    public AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            sound.Play();
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void OnStartGame()
+    {
+    }
+}
