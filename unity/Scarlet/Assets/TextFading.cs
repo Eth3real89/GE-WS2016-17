90f8a3b9761344bae028d61891008235443f4b6d
Jürgen Hahn <eth3real89@gmail.com>
2016-11-28 15:34:55 +0100

    “press any key to start” text now switches from visible to invisible each second

---
+++ unity/Scarlet/Assets/TextFading.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TextFading : MonoBehaviour {
+
+    private Text t;
+    private bool isBlinking = false;
+
+	// Use this for initialization
+	void Start () {
+	    t = GetComponent<Text>();
+
+        t.enabled = true;
+
+        StartBlinking();
+	}
+
+    public void StartBlinking()
+    {
+        if(isBlinking)
+        {
+            return;
+        }
+
+        if(t != null)
+        {
+            isBlinking = true;
+
+            InvokeRepeating("ToggleState", 0.5f, 1f);
+        }
+    }
+
+    public void ToggleState()
+    {
+        t.enabled = !t.enabled;
+    }
+
+	// Update is called once per frame
+	void Update () {
+ 
+	}
+}
