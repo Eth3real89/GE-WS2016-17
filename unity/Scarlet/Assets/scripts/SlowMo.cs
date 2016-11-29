2e714c11925eb853c972353a957407ca8f90f9ec
Jürgen Hahn <eth3real89@gmail.com>
2016-11-29 09:42:11 +0100

    added slow motion possibility (perfect parry?) which is in _MainObject und currently disabled; for testing: activate it and then press “Fire1” (left mouse or X on controller)

---
+++ unity/Scarlet/Assets/scripts/SlowMo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowMo : MonoBehaviour {
+
+    public float slowDuration;
+    public float slowAmount;
+
+    float currentSlowMo = 0f;
+
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+
+            if(Time.timeScale == 1.0f)
+                Time.timeScale = slowAmount;
+            else
+                Time.timeScale = 1.0f;
+
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        }
+
+        if(Time.timeScale == slowAmount)
+        {
+            currentSlowMo += Time.deltaTime;
+
+        }
+
+        if(currentSlowMo >= slowDuration)
+        {
+            currentSlowMo = 0f;
+
+            Time.timeScale = 1.0f;
+        }
+	}
+}
