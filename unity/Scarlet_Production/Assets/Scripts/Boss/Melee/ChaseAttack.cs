f01d8b394e58eaaf7e3adb7295b74fadebabbba8
Jonathan Brem <jon.brem@t-online.de>
2016-12-15 00:24:44 +0100

    Added Run animation & Turn behaviour to skeleton during chase

--- unity/Scarlet_Production/Assets/Scripts/Boss/Melee/ChaseAttack.cs
+++ unity/Scarlet_Production/Assets/Scripts/Boss/Melee/ChaseAttack.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 public class ChaseAttack : BossAttack {
 
     public BossMoveCommand m_BossMove;
+    public TurnTowardsScarlet m_BossTurn;
     public BossMeleeHitCommand m_BossHit;
 
     public GameObject m_Target;
 
-    private enum AttackState {None, Chase, Attack};
+    private enum AttackState {None, Chase, Turn, Attack};
     private AttackState m_State = AttackState.None;
 
     public override void StartAttack()
     {
         m_State = AttackState.Chase;
+        m_BossTurn.m_TurnSpeed = 120;
     }
 
     void Update ()
@@ -23,6 +25,10 @@ public class ChaseAttack : BossAttack {
 		if (m_State == AttackState.Chase)
         {
             Chase();
+        }
+        else if (m_State == AttackState.Turn)
+        {
+            Turn();
         }
 	}
 
@@ -32,13 +38,35 @@ public class ChaseAttack : BossAttack {
 
         if (distance.magnitude >= 1)
         {
-            m_BossMove.DoMove(distance.x, distance.z);
+            m_BossTurn.DoTurn();
+
+            if (Mathf.Abs(m_BossTurn.m_AngleTowardsScarlet) <= 45)
+            {
+                m_BossMove.DoMove(distance.x, distance.z);
+                m_BossTurn.m_TurnSpeed = 60f;
+            }
+            else
+            {
+                m_State = AttackState.Turn;
+                m_BossMove.StopMoving();
+                m_BossTurn.m_TurnSpeed = 120;
+            }
         }
         else
         {
             m_BossMove.StopMoving();
             m_State = AttackState.Attack;
             StartHit();
+        }
+    }
+
+    void Turn()
+    {
+        m_BossTurn.DoTurn();
+
+        if (Mathf.Abs(m_BossTurn.m_AngleTowardsScarlet) <= 15)
+        {
+            m_State = AttackState.Chase;
         }
     }
 
