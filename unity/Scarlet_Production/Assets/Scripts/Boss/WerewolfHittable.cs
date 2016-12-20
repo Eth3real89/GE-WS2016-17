49febae89dda466c5421850af2c2da0c59b5b894
Jonathan Brem <jon.brem@t-online.de>
2016-12-20 23:46:09 +0100

    Added blood trail and on hit signal
    more or less just copied it from the prototype;
    it gets called in a somewhat more elegant fashion, but is still not coded all that nicely.

--- unity/Scarlet_Production/Assets/Scripts/Boss/WerewolfHittable.cs
+++ unity/Scarlet_Production/Assets/Scripts/Boss/WerewolfHittable.cs
@@ -8,12 +8,17 @@ public class WerewolfHittable : MonoBehaviour, Hittable {
     public CharacterHealth m_Health;
     private HitInterject m_Interject;
 
+    public OnHitSignal m_OnHitSignal;
+
     public void Hit(Damage damage)
     {
         if (m_Interject == null || !m_Interject.OnHit(damage))
         {
             m_Health.m_CurrentHealth -= damage.DamageAmount();
             damage.OnSuccessfulHit();
+
+            if (m_OnHitSignal != null)
+                m_OnHitSignal.OnHit();
         }
     }
 
