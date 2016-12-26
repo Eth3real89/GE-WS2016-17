40d51cc8c867e2557a3586fdb5acd95f1b2f144b
Andreas Pritschet <andreas@pritschet.eu>
2016-12-26 17:38:38 +0100

    new dynamic tracking vertically

--- unity/Scarlet_Production/Assets/CameraTracking.cs
+++ unity/Scarlet_Production/Assets/CameraTracking.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,7 +27,22 @@ public class CameraTracking : MonoBehaviour
         Vector3 pos;
         pos = ClosestPointOnLine(pathPoints[0], pathPoints[1], player.transform.position);
         pos.y = 3;
-        transform.position = Vector3.Lerp(transform.position, pos - new Vector3(0, 0, 7), .1f);
+        transform.position = Vector3.Lerp(transform.position, GetBorder(pos, pathPoints[0], pathPoints[1]) - new Vector3(0, 0, 7), .1f);
+    }
+
+    private Vector3 GetBorder(Vector3 pos, Vector3 wp1, Vector3 wp2)
+    {
+        float min = Mathf.Min(wp1.x, wp2.x);
+        float max = Mathf.Max(wp1.x, wp2.x);
+        if (pos.x > max)
+        {
+            return new Vector3(max, pos.y, pos.z);
+        }
+        if (pos.x < min)
+        {
+            return new Vector3(min, pos.y, pos.z);
+        }
+        return pos;
     }
 
     public Vector3 ClosestPointOnLine(Vector3 lineStart, Vector3 lineEnd, Vector3 pnt)
