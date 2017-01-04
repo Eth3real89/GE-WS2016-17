66d7039ef0ad282fef31046f59b48a6c5cdc30f6
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2017-01-04 22:30:28 +0100

    First design for hints in game with arrow

---
+++ unity/Scarlet_Production/Assets/Scripts/UI/TriggerHint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHint : MonoBehaviour {
+
+    public GameObject hint;
+    //public GameObject text;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(FadeTo(1.0f, 0.8f));
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(FadeTo(0.0f, 0.8f));
+        }
+    }
+
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = hint.GetComponent<SpriteRenderer>().color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+            //Color newColorText = new Color(0.706f, 0, 0, Mathf.Lerp(alpha, aValue, t));
+            hint.GetComponent<SpriteRenderer>().color = newColor;
+            //text.GetComponent<TextMesh>().color = newColorText;
+            
+            yield return null;
+        }
+    }
+}
