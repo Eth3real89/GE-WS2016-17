896a56526cb1c9c8f70f96ac10ff1a8d0fcea4fe
Andreas Pritschet <andreas@pritschet.eu>
2017-01-16 15:07:41 +0100

    Added Debugger Object which allows to switch between starting points for scarlet, changed material of floor

---
+++ unity/Scarlet_Production/Assets/PlayerPosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PlayerPosition : MonoBehaviour
+{
+    public GameObject m_Scarlet;
+    public int m_StartAtPoint;
+    public Transform[] m_StartingPoint;
+
+    void Update()
+    {
+        if (m_StartingPoint.Length != 0 && m_StartingPoint.Length >= m_StartAtPoint - 1)
+            m_Scarlet.transform.position = m_StartingPoint[m_StartAtPoint].transform.position;
+    }
+}
