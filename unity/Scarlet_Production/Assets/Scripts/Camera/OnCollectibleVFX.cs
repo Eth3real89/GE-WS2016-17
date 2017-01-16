2941d6eed9849c5cedd382379a0bb0983d38e79d
Andreas Pritschet <andreas@pritschet.eu>
2017-01-16 15:47:08 +0100

    fixed collectible vfx

--- unity/Scarlet_Production/Assets/Scripts/Camera/OnCollectibleVFX.cs
+++ unity/Scarlet_Production/Assets/Scripts/Camera/OnCollectibleVFX.cs
@@ -3,59 +3,57 @@ using System.Collections;
 
 public class OnCollectibleVFX : MonoBehaviour {
 
-	public Material m_Mat = null;
+    public Material m_Mat = null;
 
-	[Range(0.01f, 0.2f)] public float m_DarkRange = 0.1f;
-	[Range(-2.5f, -1f)] public float m_Distortion = -2f;
-	[Range(0.05f, 0.3f)] public float m_Form = 0.2f;
+    [Range(0.01f, 0.2f)] public float m_DarkRange = 0.1f;
+    [Range(-2.5f, -1f)] public float m_Distortion = -2f;
+    [Range(0.05f, 0.3f)] public float m_Form = 0.2f;
 
-	private bool m_IsActive;
-	private Vector4 m_ObjectPosition;
+    private bool m_IsActive;
+    private Vector4 m_ObjectPosition;
 
-	// Use this for initialization
-	void Start () {
-		m_Mat.SetFloat ("_DarkRange", m_DarkRange);
-		m_Mat.SetFloat ("_Distortion", m_Distortion);
-		m_Mat.SetFloat ("_Form", m_Form);
+    // Use this for initialization
+    void Start () {
+        m_Mat.SetFloat ("_DarkRange", m_DarkRange);
+        m_Mat.SetFloat ("_Distortion", m_Distortion);
+        m_Mat.SetFloat ("_Form", m_Form);
 
-		m_IsActive = false;
-	}
+        m_IsActive = false;
+    }
 
-	void OnRenderImage (RenderTexture sourceTexture, RenderTexture destTexture)
-	{
-		if(this.m_IsActive)
-		{
-			this.m_Mat.SetVector ("_Center", m_ObjectPosition);
-			Graphics.Blit (sourceTexture, destTexture, this.m_Mat);
-		}
-		else
-			Graphics.Blit (sourceTexture, destTexture);
-	}
+    void OnRenderImage (RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if(m_IsActive)
+        {
+            m_Mat.SetVector ("_Center", m_ObjectPosition);
+            Graphics.Blit (sourceTexture, destTexture, m_Mat);
+        }
+        else
+            Graphics.Blit (sourceTexture, destTexture);
+    }
 
-	public void Activate(Vector3 objectPosition)
-	{
-		this.m_IsActive = true;
+    public void Activate(Vector3 objectPosition)
+    {
+        m_IsActive = true;
+        objectPosition = ToUV(objectPosition);
+        m_ObjectPosition = new Vector4(objectPosition.x, objectPosition.y, objectPosition.z, 0f);
+    }
 
-		objectPosition = ToUV(objectPosition);
+    public void Deactivate() 
+    {
+        m_IsActive = false;
+    }
 
-		this.m_ObjectPosition = new Vector4(objectPosition.x, objectPosition.y, objectPosition.z, 0f);
-	}
+    public Vector3 ToUV(Vector3 src)
+    {
+        src.x /= Screen.width;
+        src.y /= Screen.height;
 
-	public void Deactivate() 
-	{
-		m_IsActive = false;
-	}
+        return src;
+    }
 
-	public Vector3 ToUV(Vector3 src)
-	{
-		src.x /= Screen.width;
-		src.y /= Screen.height;
-
-		return src;
-	}
-
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void Update () {
+    
+    }
 }
--- unity/Scarlet_Production/Assets/Scripts/PickUp.cs
+++ unity/Scarlet_Production/Assets/Scripts/PickUp.cs
@@ -6,13 +6,45 @@ public class PickUp : MonoBehaviour
 {
     public GameObject m_Light;
 
-    void OnTriggerEnter(Collider other)
+    private Camera m_Camera;
+    private OnCollectibleVFX m_OnCollectibleVFX;
+
+    private void Start()
     {
-        if (other.tag == "Player")
+        m_Camera = Camera.main;
+        m_OnCollectibleVFX = Camera.main.GetComponent<OnCollectibleVFX>();
+    }
+
+    private void Update()
+    {
+        HandleCollectibleVisualization();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
         {
             m_Light.SetActive(false);
             gameObject.SetActive(false);
+            m_OnCollectibleVFX.Deactivate();
             Destroy(this);
+        }
+    }
+
+    private void HandleCollectibleVisualization()
+    {
+        RaycastHit hit;
+        Physics.Raycast(m_Camera.transform.position, transform.position - m_Camera.transform.position, out hit);
+        Debug.Log(hit.transform.name);
+
+        if (GetComponent<Renderer>().isVisible && hit.transform == transform)
+        {
+            Vector3 target = m_Camera.GetComponent<Camera>().WorldToScreenPoint(transform.position);
+            m_OnCollectibleVFX.Activate(target);
+        }
+        else
+        {
+            m_OnCollectibleVFX.Deactivate();
         }
     }
 }
