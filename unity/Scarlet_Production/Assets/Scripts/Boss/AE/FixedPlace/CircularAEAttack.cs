c1bfe4d1de9301ca9b5e70775edb1d12c4c034fa
Jonathan Brem <jon.brem@t-online.de>
2017-01-22 22:55:41 +0100

    Improved Circular Attack & Added Optional Tracking

---
+++ unity/Scarlet_Production/Assets/Scripts/Boss/AE/FixedPlace/CircularAEAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularAEAttack : FixedPlaceAEAttack {
+
+    public float m_SetupTime = 2;
+    public float m_Size = 5;
+    public float m_Angle = 270;
+
+    public float m_DamageAmount;
+
+    public override void StartAttack()
+    {
+        CircularSetupVisuals visuals = (CircularSetupVisuals)m_SetupVisuals;
+        visuals.m_Angle = m_Angle;
+        visuals.m_Size = m_Size;
+        visuals.m_TimeShown = m_SetupTime;
+
+        CircularAttackVisuals attackVisuals = (CircularAttackVisuals)m_AttackVisuals;
+        attackVisuals.m_Size = m_Size;
+        attackVisuals.m_Angle = m_Angle;
+
+        m_AEDamage.m_DamageAmount = m_DamageAmount;
+
+        base.StartAttack();
+    }
+
+}
--- unity/Scarlet_Production/Assets/Scripts/Boss/AE/FixedPlace/FixedPlaceAEAttack.cs
+++ unity/Scarlet_Production/Assets/Scripts/Boss/AE/FixedPlace/FixedPlaceAEAttack.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The boss is in a fixed place and may or may not rotate an attack towards scarlet.
+/// </summary>
 public class FixedPlaceAEAttack : AEAttack, FixedPlaceSetupVisuals.SetupCallback
 {
 
@@ -17,21 +20,70 @@ public class FixedPlaceAEAttack : AEAttack, FixedPlaceSetupVisuals.SetupCallback
     public FixedPlaceAttackVisuals m_AttackVisuals;
 
     public TurnTowardsScarlet m_TurnTowardsScarlet;
+    public float m_TrackingSpeedAngles;
+    private float m_PreviousSpeed;
+
+    private IEnumerator m_AttackTimer;
+
+    private IEnumerator m_SetupTimer;
+    private bool m_SetupActive = false;
 
     public override void StartAttack()
     {
         base.StartAttack();
 
+        m_SetupActive = true;
+
+        m_PreviousSpeed = m_TurnTowardsScarlet.m_TurnSpeed;
+
+        m_TurnTowardsScarlet.m_TurnSpeed = 9999;
+        m_TurnTowardsScarlet.DoTurn();
+
+        m_TurnTowardsScarlet.m_TurnSpeed = m_TrackingSpeedAngles;
+
         m_SetupVisuals.Show(this);
+
+        m_SetupTimer = TurnDuringSetup();
+        StartCoroutine(m_SetupTimer);
+    }
+
+    private IEnumerator TurnDuringSetup()
+    {
+        while (m_SetupActive)
+        {
+            m_TurnTowardsScarlet.DoTurn();
+            yield return null;
+        }
     }
 
     public void OnSetupOver()
     {
+        m_SetupActive = false;
         m_SetupVisuals.Hide();
         m_AttackVisuals.Show();
 
-        StartCoroutine(DisableAttackTimer());
+        m_AttackTimer = DisableAttackTimer();
+        StartCoroutine(m_AttackTimer);
         m_AEDamage.Activate();
+    }
+
+    public override void CancelAttack()
+    {
+        base.CancelAttack();
+
+        m_Active = false;
+        m_SetupActive = false;
+
+        m_SetupVisuals.Hide();
+
+        if (m_AttackTimer != null)
+            StopCoroutine(m_AttackTimer);
+
+        if (m_SetupTimer != null)
+            StopCoroutine(m_AttackTimer);
+
+        m_AEDamage.m_Active = false;
+        m_AttackVisuals.Hide();
     }
 
     private IEnumerator DisableAttackTimer()
