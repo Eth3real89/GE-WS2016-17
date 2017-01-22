c5195fe01e753738d13bee80e289f1f52c6261db
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2017-01-22 20:43:03 +0100

    Tutorial Instructions logic

---
+++ unity/Scarlet_Production/Assets/Scripts/UI/TutorialPromptController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPromptController : MonoBehaviour {
+
+    public GameObject TutorialPrompt;
+    private Image[] tutorialImages;
+    private Text tutorialText;
+
+    // Use this for initialization
+    void Start () {
+        tutorialImages = TutorialPrompt.GetComponentsInChildren<Image>();
+        tutorialText = TutorialPrompt.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //ShowTutorial("B", "Parry");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //HideTutorial();
+        }
+    }
+
+    /// <summary>
+    /// Show tutorial instructions with text and button prompt. ButtonShort is A, B, X or Y. Text is the text to show.
+    /// </summary>
+    /// <param name="buttonShort"></param>
+    /// <param name="newText"></param>
+    public void ShowTutorial(string buttonShort, string newText)
+    {
+        tutorialText.text = newText;
+        tutorialImages[1].sprite = Resources.Load<Sprite>("controls-" + buttonShort + "_rot");
+        StartCoroutine(FadeTo(1.0f, 0.2f));
+    }
+
+    /// <summary>
+    /// Hide tutorial instructions
+    /// </summary>
+    public void HideTutorial()
+    {
+
+        StartCoroutine(FadeTo(0.0f, 0.2f));
+    }
+
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = tutorialText.color.a;
+        float alphaBlack = tutorialImages[0].color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            //Black should stay transparent, therefore aValue is divided by 2
+            Color newBlack = new Color(0, 0, 0, Mathf.Lerp(alphaBlack, aValue/2, t));
+            Color newColorRed = new Color(0.65f, 0, 0, Mathf.Lerp(alpha, aValue, t));
+            tutorialImages[0].color = newBlack;
+            tutorialImages[1].color = newColorRed;
+            tutorialText.color = newColorRed;
+
+            yield return null;
+        }
+        tutorialImages[0].color = new Color(0, 0, 0, aValue / 2);
+        tutorialImages[1].color = new Color(0.65f, 0, 0, aValue);
+        tutorialText.color = new Color(0.65f, 0, 0, aValue);
+    }
+}
