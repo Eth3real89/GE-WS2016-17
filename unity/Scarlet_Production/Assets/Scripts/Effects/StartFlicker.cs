24ac9c3492c1a2608d71503def8083ac4f984d59
Andreas Pritschet <andreas@pritschet.eu>
2017-02-04 12:10:08 +0100

    Improved light-flicker animation (material emission now flickers as well), also added flicker-sounds

--- unity/Scarlet_Production/Assets/Scripts/Effects/StartFlicker.cs
+++ unity/Scarlet_Production/Assets/Scripts/Effects/StartFlicker.cs
@@ -7,16 +7,28 @@ public class StartFlicker : MonoBehaviour
 
     public void InitFlicker()
     {
-        StartCoroutine(Flicker());
+        StartCoroutine(FlickerLight());
+        StartCoroutine(FlickerSound());
     }
 
-    IEnumerator Flicker()
+    IEnumerator FlickerLight()
     {
         Animation[] animations = GetComponentsInChildren<Animation>();
-        for (int i = 0; i < animations.Length; i = i + 2)
+        for (int i = 0; i < animations.Length; i = i + 3)
         {
             animations[i].Play();
             animations[i + 1].Play();
+            animations[i + 2].Play();
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+        }
+    }
+
+    IEnumerator FlickerSound()
+    {
+        AudioSource[] audio = GetComponentsInChildren<AudioSource>();
+        for (int i = 0; i < audio.Length; i = i + 1)
+        {
+            audio[i].Play();
             yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
         }
     }
