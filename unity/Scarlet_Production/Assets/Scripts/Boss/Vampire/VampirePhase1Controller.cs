8622973a0829b72ff8b3b97168919fc029be1705
Jonathan Brem <jon.brem@t-online.de>
2017-02-05 02:21:07 +0100

    Continued work on Phase 1 (the parts after the tutorial)
    ... not nearly finished, though. but the attacks are there, more or less.

--- unity/Scarlet_Production/Assets/Scripts/Boss/Vampire/VampirePhase1Controller.cs
+++ unity/Scarlet_Production/Assets/Scripts/Boss/Vampire/VampirePhase1Controller.cs
@@ -1,13 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VampirePhase1Controller : VampireController
 {
+
+    public CharacterHealth m_BossHealth;
+
+    public Transform m_Refuge;
+
+    private bool m_EndInitialized;
     
     public override void StartPhase(BossfightCallbacks callbacks)
     {
+        m_EndInitialized = false;
         base.StartPhase(callbacks);
         StartCoroutine(StartAfterDelay());
     }
+
+    private void Update()
+    {
+        if (m_BossHealth.m_CurrentHealth == 0 && !m_EndInitialized)
+        {
+            if (m_ActiveCombo != null)
+                m_ActiveCombo.CancelCombo();
+
+            DashTo(m_Refuge, 1f);
+            StartCoroutine(EndPhase());
+            m_EndInitialized = true;
+        }
+    }
+
+    private IEnumerator EndPhase()
+    {
+        yield return new WaitForSeconds(1f);
+        float t = 0;
+        while((t += Time.deltaTime) < 1.5f)
+        {
+            m_BossHealth.m_CurrentHealth += m_BossHealth.m_MaxHealth * (t / 1.5f);
+            yield return null;
+        }
+        m_BossHealth.m_CurrentHealth = m_BossHealth.m_MaxHealth;
+
+        m_Callback.PhaseEnd(this);
+    }
+
+    public override void OnComboEnd(AttackCombo combo)
+    {
+        if (m_EndInitialized)
+            return;
+
+        base.OnComboEnd(combo);
+    }
+
+
 }
