328ca933cf04f24b32276df6a08395766c2736a6
Andreas Pritschet <andreas@pritschet.eu>
2017-02-12 03:27:12 +0100

    level is now basically playable

---
+++ unity/Scarlet_Production/Assets/EnableBridgeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBridgeTrigger : Interactor
+{
+    public GameObject m_OtherInteractor;
+
+    public override void Interact()
+    {
+        m_OtherInteractor.SetActive(true);
+        Destroy(gameObject);
+    }
+}
---
+++ unity/Scarlet_Production/Assets/TriggerBridge.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBridge : Interactor
+{
+    public Rigidbody m_Bridge;
+
+    public override void Interact()
+    {
+        m_Bridge.isKinematic = false;
+    }
+}
