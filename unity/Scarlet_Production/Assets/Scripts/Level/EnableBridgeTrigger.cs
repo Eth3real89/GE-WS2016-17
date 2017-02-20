686000a52e07c7c9ab0cc96c974049f7ed4a4ad7
Andreas Pritschet <andreas@pritschet.eu>
2017-02-20 21:18:45 +0100

    Fixed guide materials and placed guide in parallelworld

---
+++ unity/Scarlet_Production/Assets/Scripts/Level/EnableBridgeTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBridgeTrigger : Interactor
+{
+    public GameObject m_OtherInteractor;
+
+    public override void Interact()
+    {
+        GetComponentInChildren<UIItemPickupController>().OnItemPickedUp();
+        m_OtherInteractor.SetActive(true);
+        Destroy(gameObject);
+    }
+}
---
+++ unity/Scarlet_Production/Assets/Scripts/Level/WorldSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class WorldSwitcher : MonoBehaviour
+{
+    public delegate void SwitchWorldCallback();
+
+    public GameObject m_RealWorld, m_ParallelWorld;
+
+    private bool hasTriggered;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered)
+            return;
+
+        if (other.tag == "Player")
+        {
+            EffectController.Instance.SwitchWorld(new SwitchWorldCallback(SwitchWorld));
+            hasTriggered = true;
+        }
+    }
+
+    public void SwitchWorld()
+    {
+        if (m_RealWorld.activeSelf)
+        {
+            m_RealWorld.SetActive(false);
+            m_ParallelWorld.SetActive(true);
+        }
+        else
+        {
+            m_RealWorld.SetActive(true);
+            m_ParallelWorld.SetActive(false);
+        }
+    }
+}
