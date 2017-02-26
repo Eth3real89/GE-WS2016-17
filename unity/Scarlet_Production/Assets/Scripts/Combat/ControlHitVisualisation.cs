9e23c06c4446ec2e3a65a713f5718419fa4f72eb
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2017-02-26 16:58:20 +0100

    First attempt for hit visualisation

---
+++ unity/Scarlet_Production/Assets/Scripts/Combat/ControlHitVisualisation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attach to Character to control visualisation of hits. 
+/// </summary>
+public class ControlHitVisualisation : MonoBehaviour {
+
+    private TrailRenderer[] m_Trails;
+
+    // Use this for initialization
+    void Start () {
+        m_Trails = gameObject.transform.Find("Model").GetComponentsInChildren<TrailRenderer>();
+    }
+
+    public void EnableVisualisation()
+    {
+        foreach (TrailRenderer trail in m_Trails)
+        {
+            trail.enabled = true;
+        }
+    }
+
+    public void DisableVisualisation()
+    {
+        foreach (TrailRenderer trail in m_Trails)
+        {
+            trail.enabled = false;
+        }
+    }
+
+}
