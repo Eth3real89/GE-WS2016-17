0e2b1112fdb4c85754e30f59012b6602993d9de2
Jonathan Brem <jon.brem@t-online.de>
2017-02-28 02:00:12 +0100

    Adjustments for various values of DH attacks (bullet speed, shockwave reach)
    Also: Bullet Grenade movement works now :D

--- unity/Scarlet_Production/Assets/Scripts/Boss/Bullets/Movement/BulletGrenadeMovement.cs
+++ unity/Scarlet_Production/Assets/Scripts/Boss/Bullets/Movement/BulletGrenadeMovement.cs
@@ -22,38 +22,57 @@ public class BulletGrenadeMovement : BulletMovement
     // only "y"-plane distance is measured
 
     protected float m_TotalDistance;
-    protected float m_Distance;
 
+    protected Vector3 m_InitialPos;
     protected Vector3 m_XZMovement;
-    protected float m_YDistancePerSecond;
+    protected float m_YDistanceTotal;
     protected float m_InitialY;
+
+    protected bool m_JustDrop = false;
 
     public override void HandleMovement(BulletBehaviour b)
     {
-        if (!m_Started)
+        if (m_JustDrop)
+        {
+            m_Time += Time.deltaTime;
+            b.transform.position = Vector3.Lerp(b.transform.position, m_Goal.transform.position, Mathf.Min(m_Time, 1f));
+        }
+        else if (!m_Started)
         {
             m_Started = true;
             m_TotalDistance = Vector3.Distance(b.transform.position - new Vector3(0, b.transform.position.y, 0), m_Goal.transform.position - new Vector3(0, m_Goal.transform.position.y, 0));
 
-            m_Distance = 0;
+            if (m_TotalDistance < 2)
+            {
+                m_JustDrop = true;
+            }
+
             m_Time = 0;
 
+            m_InitialPos = b.transform.position + new Vector3();
             m_InitialY = b.transform.position.y;
 
             m_XZMovement = m_Goal.transform.position - b.transform.position;
-            m_YDistancePerSecond = m_XZMovement.y / m_TimeToReachGoal * 50;
+            m_YDistanceTotal = m_XZMovement.y;
 
             m_XZMovement.y = 0;
         }
         else if (m_Time < m_TimeToReachGoal)
         {
-            m_Time += Time.deltaTime;
+            float dt = Time.deltaTime;
+            m_Time += dt;
 
-            float xzDist = m_TotalDistance * (Time.deltaTime / m_TimeToReachGoal);
-            m_Distance += xzDist;
+            m_Time = Mathf.Min(m_Time, m_TimeToReachGoal);
 
-            Vector3 newPos = b.transform.position + m_XZMovement.normalized * xzDist;
-            newPos.y = m_InitialY + (m_YDistancePerSecond * Time.deltaTime) + Mathf.Sin(m_Time / m_TimeToReachGoal * Mathf.PI) * 2f;
+            float xzDist = m_TotalDistance * (m_Time / m_TimeToReachGoal);
+
+            Vector3 newPos = m_InitialPos + m_XZMovement.normalized * xzDist;
+            newPos.y = m_InitialY + (m_YDistanceTotal * m_Time / m_TimeToReachGoal) + Mathf.Sin(m_Time / m_TimeToReachGoal * Mathf.PI) * 3f;
+
+            if (m_YDistanceTotal < 0 && newPos.y < m_Goal.transform.position.y)
+            {
+                newPos.y = m_Goal.transform.position.y;
+            }
 
             b.transform.position = newPos;
         }
