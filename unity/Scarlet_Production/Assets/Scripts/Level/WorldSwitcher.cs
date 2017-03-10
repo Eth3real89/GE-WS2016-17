54e2b2a1aa67ccff625ee9c5f63d8f386b8d6257
Andreas Pritschet <andreas@pritschet.eu>
2017-03-10 18:41:54 +0100

    fixed portal bug

--- unity/Scarlet_Production/Assets/Scripts/Level/WorldSwitcher.cs
+++ unity/Scarlet_Production/Assets/Scripts/Level/WorldSwitcher.cs
@@ -7,22 +7,30 @@ public class WorldSwitcher : MonoBehaviour
 
     public GameObject m_RealWorld, m_ParallelWorld;
 
-    private bool hasTriggered;
+    private GameObject m_Player;
+    private Transform m_Target;
+    private DontGoThroughThings m_Blocker;
+
+    private void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+        m_Blocker = m_Player.GetComponent<DontGoThroughThings>();
+        m_Target = transform.GetChild(0);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (hasTriggered)
-            return;
-
         if (other.tag == "Player")
         {
             EffectController.Instance.SwitchWorld(new SwitchWorldCallback(SwitchWorld));
-            hasTriggered = true;
         }
     }
 
     public void SwitchWorld()
     {
+        m_Player.transform.position = m_Target.position;
+        m_Blocker.ResetPosition();
+
         if (m_RealWorld.activeSelf)
         {
             m_RealWorld.SetActive(false);
