006a91e891959dcd988a87271b3cbdbf530b51fc
Andreas Pritschet <andreas@pritschet.eu>
2017-03-12 22:37:17 +0100

    Postwerewolf Demo Scene - has to be migrated into bossfight scene

---
+++ unity/Scarlet_Production/Assets/Scripts/TalkWithWerewolf.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkWithWerewolf : Interactor
+{
+    [System.Serializable]
+    public struct TimeMarker
+    {
+        public float begin;
+        public float end;
+    }
+
+    public string audioID;
+    public List<TimeMarker> marker;
+
+    private int currentDialog;
+    private bool isInteracting;
+
+    public override void Interact()
+    {
+        if (isInteracting)
+            return;
+        isInteracting = true;
+        new FARQ().ClipName(audioID).StartTime(marker[currentDialog].begin).
+            EndTime(marker[currentDialog].end).Location(Camera.main.transform).OnFinish(StopInteraction).Play();
+            currentDialog++;
+    }
+
+    private void StopInteraction()
+    {
+        if (currentDialog == marker.Count)
+        {
+            GetComponent<Animator>().SetTrigger("Die");
+            Destroy(this);
+        }
+        isInteracting = false;
+    }
+}
