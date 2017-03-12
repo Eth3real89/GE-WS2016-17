6e616cda7d66f38e6e4df6ed2e306bb0dc0b34d3
Jürgen Hahn <eth3real89@gmail.com>
2017-03-12 17:50:32 +0100

    armour now temporarily destroys the floor where the shield and sword hits it

---
+++ unity/Scarlet_Production/Assets/Scripts/Combat/ArmourFairyGroundHitVisualisation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmourFairyGroundHitVisualisation : MonoBehaviour {
+
+    public GameObject m_GroundHitEffects;
+
+    private Component[] m_Comps;
+
+	// Use this for initialization
+	void Start () {
+        m_Comps = m_GroundHitEffects.GetComponentsInChildren<ParticleSystem>(true);
+
+        m_GroundHitEffects.transform.parent = transform;
+
+        m_GroundHitEffects.transform.localPosition = new Vector3(-2.0f, 0.0f, 5.8f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // play the effects for sword or shield hitting the ground
+    void OnAttackHitsGround() 
+    {
+        m_GroundHitEffects.transform.parent = null;
+
+        foreach(ParticleSystem ps in m_Comps)
+        {
+            ps.Play();
+        }
+
+        StartCoroutine(StopParticleSystems()); 
+    }
+
+    private IEnumerator StopParticleSystems() 
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        foreach(ParticleSystem ps in m_Comps)
+        {
+            ps.Stop();
+        }
+
+        m_GroundHitEffects.transform.parent = transform;
+        m_GroundHitEffects.transform.localPosition = new Vector3(-2.0f, 0.0f, 5.8f);
+    }
+}
