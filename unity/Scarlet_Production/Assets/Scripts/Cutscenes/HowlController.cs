2bbbb5315467b761d9d30573162fe5c747ceb7c7
Andreas Pritschet <andreas@pritschet.eu>
2017-03-18 02:33:58 +0100

    pre-werewolf cutscene

--- unity/Scarlet_Production/Assets/Scripts/Cutscenes/HowlController.cs
+++ unity/Scarlet_Production/Assets/Scripts/Cutscenes/HowlController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class HowlController : MonoBehaviour
 {
+	public void Talk()
+	{
+		new FARQ().ClipName("werewolf").StartTime(12.8f).EndTime(28.2f).Location(Camera.main.transform).Play();
+	}
+
 	public void Howl()
 	{
 		GetComponent<Animator>().SetTrigger("HowlTrigger");
+		new FARQ().ClipName("werewolf").StartTime(48.2f).EndTime(55.4f).Location(Camera.main.transform).Play();
 	}
 
 	public void Enable()
