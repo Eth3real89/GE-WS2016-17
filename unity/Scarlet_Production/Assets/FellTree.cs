7a016e987f906e738388744586104aa933481160
Andreas Pritschet <andreas@pritschet.eu>
2017-03-19 21:15:05 +0100

    Postwerewolf Realworld

---
+++ unity/Scarlet_Production/Assets/FellTree.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FellTree : Interactor
+{
+    public GameObject[] m_Objects;
+
+    public override void Interact()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        StartCoroutine(SwitchStuffOff());
+    }
+
+    IEnumerator SwitchStuffOff()
+    {
+        yield return new WaitForSeconds(1);
+        foreach (GameObject go in m_Objects)
+        {
+            Destroy(go);
+        }
+    }
+}
---
+++ unity/Scarlet_Production/Assets/OpenDoorMaze.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorMaze : Interactor
+{
+    public override void Interact()
+    {
+        GetComponent<LoadBossFight>().LoadNextScene();
+    }
+}
