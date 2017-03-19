e365447c0184f350ea90a5b5b32da95db7bd83b4
Andreas Pritschet <andreas@pritschet.eu>
2017-03-19 15:24:36 +0100

    Added HealthUpgrade

---
+++ unity/Scarlet_Production/Assets/HealthUpgrade.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HealthUpgrade : Interactor
+{
+    public float m_BuffAmount = 20;
+
+    public override void Interact()
+    {
+        CharacterHealth health = FindObjectOfType<CharacterHealth>();
+        float currentMaxHP = PlayerPrefs.GetFloat("MaxHP", health.m_MaxHealth);
+        PlayerPrefs.SetFloat("MaxHP", currentMaxHP + m_BuffAmount);
+        EffectController.Instance.EmpoweredPeak();
+        Destroy(this);
+    }
+}
