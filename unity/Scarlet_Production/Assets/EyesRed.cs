c5c504ffcbcefce2272eaccb81fe614fb29c5e8f
Andreas Pritschet <andreas@pritschet.eu>
2017-03-19 15:04:10 +0100

    pre vampire cutscene - city until post werewolf scene should now be fully playable in theory

---
+++ unity/Scarlet_Production/Assets/EyesRed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesRed : MonoBehaviour
+{
+    public void TurnEyesRed()
+    {
+        StartCoroutine(FadeEyesRed());
+    }
+
+    IEnumerator FadeEyesRed()
+    {
+        Renderer body = GetComponent<Renderer>();
+        LerpTimer t = new LerpTimer(0.2f);
+        t.Start();
+        while (t.GetLerpProgress() < 1)
+        {
+            Color c = Color.Lerp(Color.black, Color.white, t.GetLerpProgress());
+            body.materials[3].SetColor("_Color", c);
+            body.materials[3].SetColor("_EmissionColor", c);
+            yield return null;
+        }
+    }
+}
---
+++ unity/Scarlet_Production/Assets/WalkVampire.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkVampire : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Animator>().SetFloat("Speed", 1);
+        new FARQ().ClipName("vampire").StartTime(42.2f).EndTime(65.4f).Location(Camera.main.transform).Play();
+    }
+}
