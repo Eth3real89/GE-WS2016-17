4c70b720cb00007e5a27fa8479b3f2ca9979d36e
Jonathan Brem <jon.brem@t-online.de>
2017-03-19 23:14:07 +0100

    Scarlet's Health can be loaded dynamically
    Also, something I forgot to save before :(

---
+++ unity/Scarlet_Production/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : CharacterHealth {
+
+    protected override void Start()
+    {
+        m_CurrentHealth = PlayerPrefs.GetInt("MaxHP", (int) m_MaxHealth);
+        m_MaxHealth = m_CurrentHealth;
+        m_HealthStart = m_CurrentHealth;
+        m_HealthOld = m_CurrentHealth;
+    }
+
+}
