18c5162d22ae90ba85c9bb47b514d999153167ef
Jürgen Hahn <eth3real89@gmail.com>
2017-03-27 09:17:20 +0200

    added cage break effect and adjusted visuals of credits scene

--- unity/Scarlet_Production/Assets/TriggerCreditsCutscene.cs
+++ unity/Scarlet_Production/Assets/TriggerCreditsCutscene.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class TriggerCreditsCutscene : MonoBehaviour
 {
-    public GameObject earthAura, scarletAura;
+    public GameObject earthAura, scarletAura, m_CageBreakEffect;
     public Animation lightAnim;
 
     public void TriggerCutscene()
     {
         SequencedActionController.Instance.PlayCutscene("Credits");
+
+        // HERE!
+
+        m_CageBreakEffect.SetActive(true);
     }
 
     public void EnabledEarthAura()
