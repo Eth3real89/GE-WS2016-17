09f70d0816edfd08bcfbce3a34434afbd420d751
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2017-03-28 17:15:33 +0200

    fixed new game and continue while in city scene

--- unity/Scarlet_Production/Assets/Scripts/UI/MainMenuController.cs
+++ unity/Scarlet_Production/Assets/Scripts/UI/MainMenuController.cs
@@ -17,19 +17,18 @@ public class MainMenuController : MonoBehaviour
     private int selected;
     private CameraTracking cameraTracking;
     private TrackingBehaviour previousTracking;
+    private TrackingBehaviour normalTracking;
+    private bool m_FirstStartWhileSceneOpen = true;
 
     void Start()
     {
         AudioListener.volume = PlayerPrefs.GetFloat("CurrentVolume", 1);
         cameraTracking = Camera.main.GetComponent<CameraTracking>();
-
-        //if (SceneManager.GetActiveScene().name.Equals("city_exploration_level"))
-        //{
-        //    Activate();
-        //}
+        
         if (isShowing && PlayerPrefs.GetInt("IsStarted") == 0)
-        {
+            {
             previousTracking = cameraTracking.m_TrackingBehaviour;
+            normalTracking = previousTracking;
             cameraTracking.m_TrackingBehaviour = menuCamera;
             
             selected = 0;
@@ -137,7 +136,13 @@ public class MainMenuController : MonoBehaviour
     private void ZoomToScarlet()
     {
         isShowing = false;
-        cameraTracking.m_TrackingBehaviour = previousTracking;
+        if(normalTracking != null)
+        {
+            cameraTracking.m_TrackingBehaviour = normalTracking;
+        } else
+        {
+            cameraTracking.m_TrackingBehaviour = previousTracking;
+        }
         SetScarletControlsEnabled(true);
     }
 
@@ -148,8 +153,9 @@ public class MainMenuController : MonoBehaviour
             // Reset Player Prefs except volume
             PlayerPrefs.DeleteAll();
             PlayerPrefs.SetFloat("CurrentVolume", AudioListener.volume);
-            if (SceneManager.GetActiveScene().name.Equals("city_exploration_level"))
+            if (SceneManager.GetActiveScene().name.Equals("city_exploration_level") && m_FirstStartWhileSceneOpen)
             {
+                m_FirstStartWhileSceneOpen = false;
                 ZoomToScarlet();
                 Menu.SetActive(false);
                 GetComponent<MainMenuController>().enabled = false;
@@ -183,10 +189,10 @@ public class MainMenuController : MonoBehaviour
                 GetComponentInParent<IngameMenuController>().enabled = true;
 
                 ActivateScarletControls();
-                if (currentScene.Equals("city_exploration_level"))
-                {
-                    SequencedActionController.Instance.PlayCutscene("Opening");
-                }
+                //if (currentScene.Equals("city_exploration_level"))
+                //{
+                //    SequencedActionController.Instance.PlayCutscene("Opening");
+                //}
             }
         }
     }
