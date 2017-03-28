ad3cc338688be9a633fcf06d2cd764ae61201fe7
Andreas Pritschet <andreas@pritschet.eu>
2017-03-28 09:50:06 +0200

    bars now stay on floor

---
+++ unity/Scarlet_Production/Assets/RegisterBarsEvents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterBarsEvents : MonoBehaviour
+{
+    public bool addForce;
+
+    private Rigidbody rb;
+    private Collider col;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+
+        CaveEntrance.explodeEvent += Explode;
+        CaveEntrance.dropEvent += Drop;
+        CaveEntrance.destroyEvent += Remove;
+    }
+
+    private void Explode()
+    {
+        rb.isKinematic = false;
+        if (addForce)
+        {
+            Vector3 origin = GameObject.FindGameObjectWithTag("Player").transform.position;
+            rb.AddExplosionForce(500, origin, 50);
+        }
+    }
+
+    private void Drop()
+    {
+        rb.isKinematic = true;
+        col.enabled = false;
+    }
+
+    private void Remove()
+    {
+         // don't remove
+    }
+}
